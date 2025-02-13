833b8471fb26e165cd668f05c81c433ff54bf632
Wassim Tout <>
2025-02-13 20:30:55 +0000

    Updated the .play.sh to remove the output redirection to /dev/null Updated the .plays.sh to ensure the container is killed before the new automation process starts Moved the definition of CRVAULT from .play.sh to common_parameters.sh Updated the run_playbook function to set the container environment variable, MYINVOKER, during the 'podman exec' command invocation Standardized the naming convention for the files capturing process outputs Deleted the get_creds_vault_pass.sh file as it is not applicable for this automation

--- Bash/common_parameters.sh
+++ Bash/common_parameters.sh
@@ -9,6 +9,7 @@ ANSIBLE_VERSION='9.6.1'
 ANSIBLE_VARS="vars/datacenters.yml"
 PASSVAULT="vars/passwords.yml"
 REPOVAULT="vars/.repovault.yml"
+CRVAULT="vars/.bastion.yml"
 CONTAINERWD="/home/ansible/$(basename ${PWD})"
 CONTAINERREPO="registry-1.docker.io/wtout/ansible"
 SECON=$([[ "$(git config --file .git/config user.name|cut -d ' ' -f2 | tr '[:upper:]' '[:lower:]')" == "tout" ]] && echo "false" || echo "true")

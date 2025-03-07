ef0ef89cf4d564f31b5a2ec08d07077a378b65e2
Wassim Tout <>
2025-03-07 12:16:28 +0000

    Updated .plays.sh to check if MYINVOKER is set in ~/.bashrc Updated the get_common_vault_pass.sh script to ensure the password is what it should be Removed the Ansible version from from the container name in .play.sh Updated the view_vault function to allow it to change the file vault permissions Added an exit statement in view_vault to handle error situations Removed the read command from read_repo_cred function as it is not needed Updated the check_docker_login and pull_image functions to correctly handle the Ubuntu case Modified the send_notification function to support the scenario to skip the notify tag Added a new workflow to test code committed to the develop branch and create a release when successful

--- Bash/.play.sh
+++ Bash/.play.sh
@@ -17,7 +17,7 @@ INVENTORY_PATH="inventories/${ENAME}"
 SYS_DEF="Definitions/${ENAME}.yml"
 SYS_ALL="${INVENTORY_PATH}/group_vars/all.yml"
 SVCVAULT="vars/.svc_acct_creds_${ENAME}.yml"
-CONTAINERNAME="$(whoami | cut -d '@' -f1)_ansible_${ANSIBLE_VERSION}_${ENAME}"
+CONTAINERNAME="$(whoami | cut -d '@' -f1)_${ENAME}"
 NEW_ARGS=$(clean_arguments '--envname' "${ENAME}" "${@}")
 NEW_ARGS=$(clean_arguments '--avmlist' "${ALIST}" "${NEW_ARGS}")
 check_deffile
@@ -26,6 +26,7 @@ for cnum in {1..3}
 do
 	check_container "${CONTAINERNAME}_${cnum}" && kill_container "${CONTAINERNAME}_${cnum}"
 done
+# Phase 1
 start_container "${CONTAINERNAME}_1"
 [[ $- =~ x ]] && debug=1 && [[ "${SECON}" == "true" ]] && set +x
 chmod 644 "${PASSVAULT}"
@@ -36,8 +37,9 @@ PROXY_ADDRESS=$(get_proxy) || PA=${?}
 [[ ${debug} == 1 ]] && set -x
 [[ $- =~ x ]] && debug=1 && [[ "${SECON}" == "true" ]] && set +x
 rm -f "${SVCVAULT}"; umask 0022; touch "${SVCVAULT}"
-[[ "$(echo ${PCREDS_LIST})" != "" ]] && echo "${PCREDS_LIST[@]}" | sed "s/$(get_creds_prefix primary)/P/g; s/^\(.*: \)\(.*\)$/\1'\2'/g" >> "${SVCVAULT}"
-[[ "$(echo ${SCREDS_LIST})" != "" ]] && echo "${SCREDS_LIST[@]}" | sed "s/$(get_creds_prefix secondary)/S/g; s/^\(.*: \)\(.*\)$/\1'\2'/g" >> "${SVCVAULT}"
+[[ "$(echo ${PCREDS_LIST})" != "" ]] && echo "${PCREDS_LIST[@]}" | sed "s/$(get_creds_prefix primary)/P/g; s/^\(.*: \)\(.*\)$/\1'\2'/g" >> "${SVCVAULT}" || PCREDS_EMPTY=true
+[[ "$(echo ${SCREDS_LIST})" != "" ]] && echo "${SCREDS_LIST[@]}" | sed "s/$(get_creds_prefix secondary)/S/g; s/^\(.*: \)\(.*\)$/\1'\2'/g" >> "${SVCVAULT}" || SCREDS_EMPTY=true
+[[ ${PCREDS_EMPTY} && ${SCREDS_EMPTY} ]] && echo "Service creds not found" && Exit 1
 [[ ${debug} == 1 ]] && set -x
 add_write_permission "${SVCVAULT}"
 encrypt_vault "${CONTAINERNAME}_1" "${SVCVAULT}" Bash/get_common_vault_pass.sh
@@ -47,13 +49,15 @@ get_inventory "${CONTAINERNAME}_1" "${@}"
 get_hosts "${CONTAINERNAME}_1" "${@}"
 NUM_HOSTSINPLAY=$([[ "$(get_hostsinplay "${CONTAINERNAME}_1" "${HL}" | wc -w)" != "0" ]] && get_hostsinplay "${CONTAINERNAME}_1" "${HL}" | wc -w || echo "1")
 create_symlink
+kill_container "${CONTAINERNAME}_1"
+# Phase 2
 enable_logging "${CONTAINERNAME}_2" "${@}"
 start_container "${CONTAINERNAME}_2"
-kill_container "${CONTAINERNAME}_1"
 run_playbook "${CONTAINERNAME}_2" "${@}"
-disable_logging
-start_container "${CONTAINERNAME}_3"
 kill_container "${CONTAINERNAME}_2"
+disable_logging
+# Phase 3
+start_container "${CONTAINERNAME}_3"
 send_notification "${CONTAINERNAME}_3" "${ORIG_ARGS}"
 kill_container "${CONTAINERNAME}_3"
 exit ${SCRIPT_STATUS}

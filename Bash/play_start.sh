d7cd8a7c0d5e20ef59572956436419eae83cfc2f
Wassim Tout <>
2025-04-20 18:09:02 +0000

    Replaced the release.yml workflow with two new workflows:   ci.yml workflow to run tests anytime there is a commit to the develop, feature/*, and hotfix/* branches. It also merges the feature and hotfix branches to their corresponding destination branch   rba.yml workflow to run tests anytime there is a commit to the release/* branch. It also merges the release branch to both master and develop branches Updated the Bash and yaml code to comply with the coding best practices outlined in shellcheck and ansible-lint Updated the get_secrets_vault function to default localbranch to develop when dealing with feature or release branches Updated the get_secrets_vault function to default localbranch to master when dealing with a hotfix branch

--- Bash/play_start.sh
+++ Bash/play_start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # Usage sh Bash/play_start.sh
 
-source $(dirname "${0}")/.plays.sh
+source "$(dirname "${0}")/.plays.sh"
--- Bash/play_stop.sh
+++ Bash/play_stop.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # Usage bash Bash/play_stop.sh
 
-source $(dirname "${0}")/.plays.sh
+source "$(dirname "${0}")/.plays.sh"
